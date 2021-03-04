29b9cc4545f529d0c88b83c0b16c5e8ea15b1002
Ana Mafla <anamafla@hotmail.com>
2021-03-04 18:11:32 -0500

    setup-store-and-render

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,13 @@
-import { render, screen } from "@testing-library/react";
+//Wimport { screen } from "@testing-library/react";
+import { render, screen } from "./custom-render";
 import App from "./App";
 
 describe("<App/>", () => {
   it("Renders <App/> component correctly", () => {
     render(<App />);
-    const linkElement = screen.getByText(
-      /Getting started with React testing library/i
-    );
-    expect(linkElement).toBeInTheDocument();
+
+    expect(
+      screen.getByText(/Getting started with React testing library/i)
+    ).toBeInTheDocument();
   });
 });
